99d611878d020dbf057770812e71a145335d6a5e
Iván Santamaría <ivansantamaria.m@gmail.com>
2017-12-19 23:08:33 +0100

    Return city from URL

---
+++ team/src/main/java/urlshortener/RedPepper/model/Checker.java
@@ -0,0 +1,20 @@
+package urlshortener.RedPepper.model;
+
+/**
+ * Created by ivansantamaria on 19/12/17.
+ */
+public class Checker {
+    String url;
+
+    public Checker(String url) {
+        this.url = url;
+    }
+
+    public IpGeoResults cityUrl(String url) {
+        IpGeoResults ipGeoResults = new IpGeoResults();
+        ipGeoResults.setCity("zaragoza");
+        ipGeoResults.setLatitude(41.39);
+        ipGeoResults.setLongitude(0.52);
+        return ipGeoResults;
+    }
+}
