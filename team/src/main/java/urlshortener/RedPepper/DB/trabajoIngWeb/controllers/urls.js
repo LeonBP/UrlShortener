d48585ba5033af8c8c9393bffee6a8be13ef7656
fiusa8 <dfusteropalacios@gmail.com>
2018-01-09 19:48:21 +0100

    Update DB

---
+++ team/src/main/java/urlshortener/RedPepper/DB/trabajoIngWeb/controllers/urls.js
@@ -0,0 +1,64 @@
+var UrlModel = require('../models/urls');
+
+//GET - Return all registers
+exports.findAll = function(req, res) {
+    UrlModel.getUrls(function(err, clients) {
+        if(err) return res.send(500, err.message);
+        console.log('GET /urls')
+        res.status(200).jsonp(clients);
+    });
+};
+
+//GET - Return a register with specified geohash
+exports.findByGeohash = function(req, res) {
+    UrlModel.getUrlGeohash(req.params.id, function(err, client) {
+        if(err) return res.send(500, err.message);
+        console.log('GET /urls/' + req.params.geohash);
+        res.status(200).jsonp(client);
+    });
+};
+
+//POST - Insert a new register
+exports.add = function(req, res) {
+    console.log('POST');
+    console.log(req.body);
+    var url = {
+        id: req.body.id,
+        url: req.body.url,
+        urlAcortada: req.body.urlAcortada,
+        clicks: req.body.clicks,
+        latitud: req.body.latitud,
+        longitud: req.body.longitud
+    };
+    UrlModel.insertUrl(url, function(err, client) {
+        if(err) return res.send(500, err.message);
+        res.status(200).jsonp(client);
+    });
+};
+
+//PUT - Update a register already exists
+exports.update = function(req, res) {
+    UrlModel.getUrl(req.params.id, function(err, url) {
+        console.log(req.body);
+        var url = {
+            id: req.params.id,
+            url: req.body.url,
+            urlAcortada: req.body.urlAcortada,
+            clicks: req.body.clicks,
+            latitud: req.body.latitud,
+            longitud: req.body.longitud
+        };
+        UrlModel.updateUrl(url, function(err) {
+            if(err) return res.send(500, err.message);
+            res.status(200).jsonp(url);
+        });
+    });
+};
+
+//DELETE - Delete a register with specified ID
+exports.delete = function(req, res) {
+    UrlModel.deleteUrl(req.params.id, function(err, url) {
+        if(err) return res.send(500, err.message);
+        res.json({ message: 'Successfully deleted' });
+    });
+};
