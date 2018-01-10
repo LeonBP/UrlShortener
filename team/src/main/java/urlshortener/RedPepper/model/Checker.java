1284ed5c15f451b5af96bfbc505c27d90ac8cc70
Iván Santamaría <ivansantamaria.m@gmail.com>
2018-01-10 23:06:59 +0100

    edit checker

--- team/src/main/java/urlshortener/RedPepper/model/Checker.java
+++ team/src/main/java/urlshortener/RedPepper/model/Checker.java
@@ -6,6 +6,7 @@ package urlshortener.RedPepper.model;
 public class Checker {
 
     String url;
+    int tipoValidacion;
 
     public Checker(String url) {
         this.url = url;
@@ -20,7 +21,41 @@ public class Checker {
         ipGeoResults.setLatitude(41.39);
 
         ipGeoResults.setLongitude(0.52);
-
         return ipGeoResults;
+    }
+
+    public void listCities() {
+        for (int i = 0; i < 100; i++) {
+            if (this.url == "a") {
+                tipoValidacion = 0;
+            }
+            else if (this.url == "url") {
+                tipoValidacion = 1;
+            }
+            else if (this.url == "shortened") {
+                tipoValidacion = 2;
+            }
+            else if (this.url == "b") {
+                tipoValidacion = 3;
+            }
+            else if (this.url == "url") {
+                tipoValidacion = 4;
+            }
+            else if (this.url == "shortened") {
+                tipoValidacion = 5;
+            }
+            else if (this.url == "a") {
+                tipoValidacion = 6;
+            }
+            else if (this.url == "url") {
+                tipoValidacion = 7;
+            }
+            else if (this.url == "shortened") {
+                tipoValidacion = 8;
+            }
+            else {
+                tipoValidacion = -1;
+            }
+        }
     }
 }
