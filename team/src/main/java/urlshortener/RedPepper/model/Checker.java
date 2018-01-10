07847b800d61f231da4e1ccd60ef7cb5ca1d878c
Iván Santamaría <ivansantamaria.m@gmail.com>
2018-01-10 23:08:47 +0100

    edit checker

--- team/src/main/java/urlshortener/RedPepper/model/Checker.java
+++ team/src/main/java/urlshortener/RedPepper/model/Checker.java
@@ -23,49 +23,4 @@ public class Checker {
         ipGeoResults.setLongitude(0.52);
         return ipGeoResults;
     }
-
-    public void listCities() {
-        for (int i = 0; i < 100; i++) {
-            if (this.url == "a")
-            {
-                tipoValidacion = 0;
-            }
-            else if (this.url == "url")
-            {
-                tipoValidacion = 1;
-            }
-            else if (this.url == "shortened")
-            {
-                tipoValidacion = 2;
-            }
-            else if (this.url == "b")
-            {
-                tipoValidacion = 3;
-            }
-            else if (this.url == "url")
-            {
-                tipoValidacion = 4;
-            }
-            else if (this.url == "shortened")
-            {
-                tipoValidacion = 5;
-            }
-            else if (this.url == "a")
-            {
-                tipoValidacion = 6;
-            }
-            else if (this.url == "url")
-            {
-                tipoValidacion = 7;
-            }
-            else if (this.url == "shortened")
-            {
-                tipoValidacion = 8;
-            }
-            else
-            {
-                tipoValidacion = -1;
-            }
-        }
-    }
 }
