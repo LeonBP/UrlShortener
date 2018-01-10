9468ff8d4a5f927b4241dd4258733307a18a8aed
Iván Santamaría <ivansantamaria.m@gmail.com>
2018-01-10 23:31:23 +0100

    check if city is already selected

--- common/src/main/java/urlshortener/common/domain/ShortURL.java
+++ common/src/main/java/urlshortener/common/domain/ShortURL.java
@@ -19,6 +19,7 @@ public class ShortURL {
 	public ShortURL(String hash, String target, URI uri, String sponsor,
 			Date created, String owner, Integer mode, Boolean safe, String ip,
 			String country) {
+
 		this.hash = hash;
 		this.target = target;
 		this.uri = uri;
@@ -35,42 +36,52 @@ public class ShortURL {
 	}
 
 	public String getHash() {
+
 		return hash;
 	}
 
 	public String getTarget() {
+
 		return target;
 	}
 
 	public URI getUri() {
+
 		return uri;
 	}
 
 	public Date getCreated() {
+
 		return created;
 	}
 
 	public String getOwner() {
+
 		return owner;
 	}
 
 	public Integer getMode() {
+
 		return mode;
 	}
 
 	public String getSponsor() {
+
 		return sponsor;
 	}
 
 	public Boolean getSafe() {
+
 		return safe;
 	}
 
 	public String getIP() {
+
 		return ip;
 	}
 
 	public String getCountry() {
+
 		return country;
 	}
 
