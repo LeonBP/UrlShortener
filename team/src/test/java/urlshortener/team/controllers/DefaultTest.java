7525b482414d9c7c32c8c82473b53e16b0313641
crzyivo <ivan.nadie@gmail.com>
2018-01-11 02:45:45 +0100

    Semi-random cities.

--- team/src/test/java/urlshortener/team/controllers/DefaultTest.java
+++ team/src/test/java/urlshortener/team/controllers/DefaultTest.java
@@ -31,24 +31,14 @@ public class DefaultTest {
     }
 
     @Test
-    public void thatGetsALocationOnCorrectIP()
+    public void thatPostAUrlOnRandomMode()
             throws Exception {
-        String expectedJson = "{\"mode\":\"1\",\"clientIP\":\"90.94.192.43\"," +
-                "\"precision\":1}";
         String bodyParams = "{\"mode\":\"1\",\"clientIP\":\"90.94.192.43\"," +
-                "\"precision\":1}";
-        mockMvc.perform(post("/").with(remoteAddr("90.94.192.43"))
+                "\"precision\":1,\"url\":\"https://www.google.es/\"}";
+        mockMvc.perform(post("/new").with(remoteAddr("90.94.192.43"))
                 .contentType(MediaType.APPLICATION_JSON).content(bodyParams)).andDo(print())
                 .andExpect(status().isOk());
 
-    }
-
-    @Test
-    public void thatBadIpGetsError404()
-            throws Exception{
-        String bodyParams = "{\"redirect\": false,\"radio\": 0,\"resultNumber\": 1,\"cosa\":\"kek\"}";
-        mockMvc.perform(post("/pinpoint").with(remoteAddr("0.0.0.1"))
-                .contentType(MediaType.APPLICATION_JSON).content(bodyParams)).andExpect(status().isNotFound());
     }
 
     private static RequestPostProcessor remoteAddr(final String remoteAddr) {
